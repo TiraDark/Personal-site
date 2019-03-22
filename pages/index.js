89ce3403a337e1c21d5938af5128a760ccf82a5a
TiraDark <48825619+TiraDark@users.noreply.github.com>
2019-03-22 16:02:42 +0100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Layout from "../layouts/default";
+
+export default () => (
+  <Layout>
+    <h1>Wellcome to the workshop</h1>
+    <p> This is a very good Übung</p>
+    <h2>Hello :+1: </h2>
+
+    <p>
+      Here is a longer paragraph. Income established fall taxpayer economy
+      inverse market exposure. Term market index IRA taxpayer municipal bonds.
+      Stocks credit potential upswing value rise money. Performance
+      capitalization expenses holder bull strategy substantially 401k
+      bondholders credit. Managed Standard & Poor's yield. Funds municipal notes
+      downturn Dow Jones hedge fund. Treasury Nikkei taxpayer index funds fall
+      rates downturn.
+    </p>
+    <h3>A h3 subheading</h3>
+    <p>
+      Yet another paragraph. Money performance expenses federal maturities
+      investment grade bonds credit quality. Volatile corporate interest rate
+      appeal.
+    </p>
+  </Layout>
+);
